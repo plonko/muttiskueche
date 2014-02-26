628c2a587a6b4f5cf9ac84cd9699ef3a3c5d5776
Seb <sebastianjthomas@yahoo.co.uk>
2014-02-26 21:37:41 +0000

    Added routing for templating

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
-var app = angular.module("kueche", ["firebase"]);
+var app = angular.module("kueche", ["ngRoute", "firebase"]);
 
 app.factory('Recipes', function($firebase) {
   return $firebase(new Firebase('https://fiery-fire-291.firebaseio.com/'));
 });
 
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller:'ListCtrl',
+            templateUrl:'list.html'
+        })
+        // .when('/edit/:projectId', {
+        //     controller:'EditCtrl',
+        //     templateUrl:'detail.html'
+        // })
+        // .when('/new', {
+        //     controller:'CreateCtrl',
+        //     templateUrl:'detail.html'
+        // })
+        .otherwise({
+            redirectTo:'/'
+    });
+});
 
 app.controller('ListCtrl', function ($scope, Recipes) {
     $scope.recipes = Recipes;
