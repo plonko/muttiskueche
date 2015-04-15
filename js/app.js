2e55e468797601b5ea891e6fa39fb82b0eed56a7
Seb <sebastian@livingmap.com>
2015-04-15 17:27:41 +0100

    Updated to work with new firebase object/array

--- js/app.js
+++ js/app.js
@@ -2,11 +2,6 @@ var app = angular.module("kueche", ["ngRoute", "firebase"]);
 
 //Firebase.enableLogging(true, true);
 
-app.value('fbURL', 'https://fiery-fire-291.firebaseio.com/');
-
-app.factory('Recipes', function($firebase, fbURL) {
-  return $firebase(new Firebase(fbURL));
-});
 
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
@@ -20,75 +15,32 @@ app.config(function($routeProvider, $locationProvider) {
     })
     .when('/new', {
         controller:'CreateCtrl',
-        templateUrl:'detail.html', 
-        isLogin: true
+        templateUrl:'detail.html'
     })
     .otherwise({
         redirectTo:'/'
     });
-    // configure html5 to get links working
-    // If you don't do this, you URLs will be base.com/#/home rather than base.com/home
-    //$locationProvider.html5Mode(true);
 });
 
-// app.run(function($rootScope, $location) {
-//     $rootScope.$on("$routeChangeStart", function(event, next) {
-//         var userAuthenticated = false; /* Check if the user is logged in */
+app.factory("Recipes", ["$firebaseArray",
+    function($firebaseArray) {
+        var ref = new Firebase("https://fiery-fire-291.firebaseio.com/");
+        return $firebaseArray(ref);
+    }
+]);
 
-//         if (!userAuthenticated && next.isLogin) {
-//              You can save the user's location to take him back to the same page after he has logged-in 
-//             //event.preventDefault();
-//             ///$rootScope.savedLocation = $location.url();
-
-//             $location.path('http://www.google.com');
-//             alert('youre not logged in');
-//         }
-//     });
-// });
-
-app.controller('LoginCtrl', function ($scope, $timeout, fbURL) {
-    var ref = new Firebase(fbURL);
-    var auth = new FirebaseSimpleLogin(ref, function(error, user) {
-        $timeout(function() {
-            if (error) {
-                $scope.error = error;
-                console.log(error.code);
-                console.log(error.message);
-            } else if (user) {
-                $scope.error = null;
-                console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
-            } else {
-                $scope.error = 'user is logged out';
-            }
-        });
-    });
-    $scope.login = function() {
-        auth.login('password', {
-            email: $scope.loginEmail,
-            password: $scope.loginPassword
-        });
-    };
-});
-
-app.controller('ListCtrl', function ($scope, Recipes) {
-    $scope.recipes = Recipes;
-});
+app.controller("ListCtrl", ["$scope", "Recipes",
+    function($scope, Recipes) {
+        $scope.recipes = Recipes;
+    }
+])
 
 app.controller('CreateCtrl', function($scope, $location, $timeout, Recipes) {
     $scope.recipe = {ingredients: [{id:1}]};
     $scope.save = function() {
         Recipes.$add($scope.recipe);
         $location.path('/');
-        // Cannot get this add callback to work!! Need to revisit. Location change works sychronously for now.
-        // function() {
-        //            console.log('ooh la la une!');
-        //            $timeout(function() {
-        //                console.log('ooh la la!');
-        //                $location.path('/');
-        //            }, 400);
-        //        }
     };
-    // Stolen from https://www.twilio.com/blog/2013/12/votr-part-5-angularjs-crud-restful-apis.html
     $scope.addIngredient = function() {
         $scope.recipe.ingredients.push({id: $scope.recipe.ingredients.length+1});
     };
@@ -102,10 +54,9 @@ app.controller('CreateCtrl', function($scope, $location, $timeout, Recipes) {
 });
 
 
-app.controller('EditCtrl',
-  function($scope, $location, $routeParams, $firebase, fbURL) {
-    var recipeUrl = fbURL + $routeParams.recipeId;
-    $scope.recipe = $firebase(new Firebase(recipeUrl));
+app.controller('EditCtrl', function($scope, $location, $routeParams, $firebaseObject) {
+    var recipeUrl = 'https://fiery-fire-291.firebaseio.com/' + $routeParams.recipeId;
+    $scope.recipe = $firebaseObject(new Firebase(recipeUrl));
 
     $scope.destroy = function() {
       $scope.recipe.$remove();
@@ -121,7 +72,6 @@ app.controller('EditCtrl',
     };
     $scope.removeIngredient = function(ingredient) {
         $scope.recipe.ingredients.splice(ingredient.id-1, 1);
-        // need to make sure id values run from 1..x (web service constraint)
         $scope.recipe.ingredients.forEach(function(ingredient, index) {
             ingredient.id = index+1;
         });
