a78551c2249938b40e887d5e3d63033bdb66edc3
Seb <sebastian@livingmap.com>
2015-04-16 17:52:40 +0100

    Added auth to routing

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 var app = angular.module("kueche", ["ngRoute", "firebase"]);
 
+app.run(["$rootScope", "$location", function($rootScope, $location) {
+    $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+        // We can catch the error thrown when the $requireAuth promise is rejected
+        // and redirect the user back to the home page
+        if (error === "AUTH_REQUIRED") {
+          $location.path("/");
+        }
+    });
+}]);
+
 //Firebase.enableLogging(true, true);
 
+app.factory("Auth", ["$firebaseAuth",
+    function($firebaseAuth) {
+        var ref = new Firebase("https://fiery-fire-291.firebaseio.com/");
+        return $firebaseAuth(ref);
+    }
+]);
 
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
@@ -11,23 +27,34 @@ app.config(function($routeProvider, $locationProvider) {
     })
     .when('/edit/:recipeId', {
         controller:'EditCtrl',
-        templateUrl:'detail.html'
+        templateUrl:'detail.html',
+        resolve: {
+        // controller will not be loaded until $waitForAuth resolves
+        // Auth refers to our $firebaseAuth wrapper in the example above
+        "currentAuth": ["Auth", function(Auth) {
+            // $waitForAuth returns a promise so the resolve waits for it to complete
+            return Auth.$requireAuth();
+        }]
+      }
     })
     .when('/new', {
         controller:'CreateCtrl',
-        templateUrl:'detail.html'
+        templateUrl:'detail.html',
+        resolve: {
+        // controller will not be loaded until $waitForAuth resolves
+        // Auth refers to our $firebaseAuth wrapper in the example above
+        "currentAuth": ["Auth", function(Auth) {
+            // $waitForAuth returns a promise so the resolve waits for it to complete
+            return Auth.$requireAuth();
+        }]
+      }
     })
     .otherwise({
         redirectTo:'/'
     });
 });
 
-app.factory("Auth", ["$firebaseAuth",
-    function($firebaseAuth) {
-        var ref = new Firebase("https://fiery-fire-291.firebaseio.com/");
-        return $firebaseAuth(ref);
-    }
-]);
+
 
 // and use it in our controller
 app.controller("LoginCtrl", ["$scope", "Auth",
@@ -67,33 +94,35 @@ app.controller("ListCtrl", ["$scope", "Recipes", "Auth",
 
         $scope.auth.$onAuth(function(authData) {
             $scope.authData = authData;
+            console.log($scope.authData)
         });
     }
 ])
 
-app.controller('CreateCtrl', function($scope, $location, $timeout, Recipes) {
-    $scope.recipe = {ingredients: [{id:1}]};
-    $scope.save = function() {
-        Recipes.$add($scope.recipe);
-        $location.path('/');
-    };
-    $scope.addIngredient = function() {
-        $scope.recipe.ingredients.push({id: $scope.recipe.ingredients.length+1});
-    };
-    $scope.removeIngredient = function(ingredient) {
-        $scope.recipe.ingredients.splice(ingredient.id-1, 1);
-        // need to make sure id values run from 1..x (web service constraint)
-        $scope.recipe.ingredients.forEach(function(ingredient, index) {
-            ingredient.id = index+1;
-        });
-    };
-});
+app.controller('CreateCtrl', ["$scope", "$location", "$timeout", "Recipes", "currentAuth",
+    function($scope, $location, $timeout, Recipes, currentAuth) {
+        $scope.recipe = {ingredients: [{id:1}]};
+        $scope.save = function() {
+            Recipes.$add($scope.recipe);
+            $location.path('/');
+        };
+        $scope.addIngredient = function() {
+            $scope.recipe.ingredients.push({id: $scope.recipe.ingredients.length+1});
+        };
+        $scope.removeIngredient = function(ingredient) {
+            $scope.recipe.ingredients.splice(ingredient.id-1, 1);
+            // need to make sure id values run from 1..x (web service constraint)
+            $scope.recipe.ingredients.forEach(function(ingredient, index) {
+                ingredient.id = index+1;
+            });
+        };
+    }
+]);
 
 
 app.controller('EditCtrl', function($scope, $location, $routeParams, $firebaseObject) {
     var recipeUrl = 'https://fiery-fire-291.firebaseio.com/' + $routeParams.recipeId;
     $scope.recipe = $firebaseObject(new Firebase(recipeUrl));
-
     $scope.destroy = function() {
       $scope.recipe.$remove();
       $location.path('/');
